3bc78e9d240917d0f69aa46f9555fea384a846e7
hayaki22 <hayaki22vn@gmail.com>
2021-08-11 19:53:23 +0200

    Test

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Hayaki Maruta on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
